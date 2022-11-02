f88f3d04ca1b06f496c7aceb75d8074a2534c65f
Hunter Wood <hunter674530@gmail.com>
2022-11-02 15:40:07 -0600

    commit tree fd09648239125e73d1457d7b1144602f028d0c79

---
+++ index.js
@@ -0,0 +1,42 @@
+function add(a,b){
+  return a+b;
+}
+add(300, 45);
+
+function subtract(a,b){
+  return a - b;
+}
+subtract(500, 80);
+
+function multiply(a,b){
+  return a*b;
+}
+multiply(84279.7, 10);
+
+function divide(a,b){
+  return a/b;
+}
+divide(11.263736263736264, 100);
+
+function increment(n){
+  n++;
+  return n;
+}
+increment(761);
+
+function decrement(n){
+  n--;
+  return n;
+}
+decrement(694);
+
+function makeInt(string){
+  return parseInt(string, 10);
+  
+}
+makeInt("707");
+
+function preserveDecimal(string){
+  return parseFloat(string);
+}
+preserveDecimal("7.945");
